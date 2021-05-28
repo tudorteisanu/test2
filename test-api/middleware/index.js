abc7ecc42456bc49cf2768d688c72452b7b8a1e3
Mihai <cronixm1@gmail.com>
2021-05-28 14:37:51 +0300

    push

--- test-api/middleware/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-localization,authorization')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    next()
-  })
-}
